c86779f55a9726e0ad8ca9e18acfd525134f87be
figure_ai <592134873qq.com>
2017-04-26 10:10:01 +0800

    初始化项目

---
+++ app/src/main/java/com/coolweather/android/db/city.java
@@ -0,0 +1,48 @@
+package com.coolweather.android.db;
+
+/**
+ * Created by anwser_mac on 2017/4/26.
+ */
+
+public class city {
+
+    private int id;
+
+    private String cityName;
+
+    private int provinceId;
+
+    private int cityCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+}
